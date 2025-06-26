d87aef2890ed9a25fddac27b89180895c8e15d53
Endryus-Montezano <endryusmontezano@live.com>
2025-06-26 18:05:21 -0300

    Aprendendo React
    Estilização com Styled-Components

--- dio/src/pages/home/index.jsx
+++ dio/src/pages/home/index.jsx
@@ -1,8 +1,12 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
 
 const Home = () => {
     return (<>
-        <h1>Home</h1>
+        <Header />
+        <Button title="Teste 1" />
+        <Button variant="secondary" title="Teste 2"/>
         <Link to="/login">Fazer login</Link>
     </>)
 }
